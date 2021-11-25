59a9984ada4ad36dcd6a16001845a9650bd363a6
YahiaAlbghdadi <r0y4lalbghdadi@gmail.com>
2021-11-25 09:29:45 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { TravelsComponent } from './travels/travels.component';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",component:ContentComponent
+},{
+  path:"blog",component:BlogComponent
+},{
+  path:"travels",component:TravelsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
